f668ae4fcd0ee5e241068e51b7c4777b77e9c52f
Timothé Boone <timotheboone@gmail.com>
2024-03-22 23:16:08 +0100

    Adds swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,23 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('LAP - API')
+    .setDescription(
+      'API used by the mobile application LAP, an application made for make inventories easier',
+    )
+    .setVersion('1.0')
+    .addTag('LAP')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
