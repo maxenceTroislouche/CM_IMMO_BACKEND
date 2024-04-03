44bdc72eddd0b44337240e825e16cd5b4a0f8bcc
timothe <timotheboone@gmail.com>
2024-04-03 12:06:14 +0200

    Adds init data for agent

---
+++ database/init/02 - agent.sql
@@ -0,0 +1,16 @@
+INSERT INTO agent (nom_utilisateur , mot_de_passe)
+VALUES ('user1', 'pass1'),
+       ('user2', 'pass2'),
+       ('user3', 'pass3'),
+       ('user4', 'pass4'),
+       ('user5', 'pass5'),
+       ('user6', 'pass6'),
+       ('user7', 'pass7'),
+       ('user8', 'pass8'),
+       ('user9', 'pass9'),
+       ('user10', 'pass10'),
+       ('user11', 'pass11'),
+       ('user12', 'pass12'),
+       ('user13', 'pass13'),
+       ('user14', 'pass14'),
+       ('user15', 'pass15');
