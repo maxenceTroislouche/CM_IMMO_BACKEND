c4886ff6cf6f698ebfb2e7d265c9343edfd8bfac
timothe <timotheboone@gmail.com>
2024-04-03 12:05:53 +0200

    Adds .env examples and env variables for JWT

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,22 +3,22 @@ import { AuthModule } from './modules/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot(
+      {envFilePath: ['.env.local','.env.development','.env']}
+    ),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async(configService: ConfigService) => ({
-        type: "postgres",
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
-        // entities: [__dirname + '/**/entities/*.entity.ts'],
-        synchronize: !configService.get<boolean>('ENV_PROD'), // Should'nt be used in production
+        synchronize: configService.get<boolean>('DB_SYNCHRONIZE'), // Should'nt be used in production
         autoLoadEntities: true,
       }),
     }),
--- api/src/modules/auth/auth.module.ts
+++ api/src/modules/auth/auth.module.ts
@@ -3,19 +3,22 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AgentsModule } from '../estateAgents/estateAgents.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './config/constants';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     AgentsModule,
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        global: configService.get<boolean>('JWT_GLOBAL'),
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRESIN') },
+      }),
     }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
-  
 })
 export class AuthModule {}
