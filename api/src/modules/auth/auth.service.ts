d64207d4f0781556a008d7cb0269c18d0ad704f7
timothe <timotheboone@gmail.com>
2024-04-24 13:50:32 +0200

    Rename usersService in estateAgentsService

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,16 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { EstateAgentsService } from '../estateAgents/estateAgents.service';
 import { JwtService } from '@nestjs/jwt';
+import { CreateEstateAgentDto } from '../estateAgents/dto/create-estateAgent.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private usersService: EstateAgentsService,
+    private estateAgentsService: EstateAgentsService,
     private jwtService: JwtService,
   ) {}
 
   async signIn(username: string, password: string): Promise<{access_token: string}> {
-    const agent = await this.usersService.findOneByUsername(username);
+    const agent = await this.estateAgentsService.findOneByUsername(username);
     if (agent?.password !== password) {
       throw new UnauthorizedException();
     }
