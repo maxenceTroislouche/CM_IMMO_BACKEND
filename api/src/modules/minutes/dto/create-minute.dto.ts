d1bc1d6abbfcfc4c8a8b70a372e84f3ed198ed38
Lolocontact <thecontactlolo@gmail.com>
2024-05-29 10:53:09 +0200

    Ajout DTO create Minute - Petitprez Loic
    Ajout de DTO create Minute

--- api/src/modules/minutes/dto/create-minute.dto.ts
+++ api/src/modules/minutes/dto/create-minute.dto.ts
@@ -1,1 +1,29 @@
-export class CreateMinuteDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsNumber, IsArray, IsOptional, IsInt } from "class-validator";
+
+export class CreateMinuteDto {
+
+    @ApiProperty()
+    @IsNumber()
+    id_edl: number;
+
+    @ApiProperty()
+    @IsNumber()
+    id_element: number;
+
+    @ApiProperty({ type: [Number], description: 'Array of photo IDs' })
+    @IsArray()
+    @IsNumber({}, { each: true })
+    photos: number[];
+
+    @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
+    remarque?: string;
+
+    @ApiProperty({ required: false })
+    @IsInt()
+    @IsOptional()
+    note?: number;
+}
+
